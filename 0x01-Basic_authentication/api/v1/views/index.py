6be668b2f6332af990f52e58e4715e24dea7a294
Liquelaliqour <lewismomanyi34@gmail.com>
2024-11-12 19:14:35 +0300

    Debugging the Method name 'get' to ensure consistency

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -25,7 +25,7 @@ def stats() -> str:
     stats['users'] = User.count()
     return jsonify(stats)
 
-@app_views.route('/unauthorized/', methods=['Get'], strict_slashes=False)
+@app_views.route('/unauthorized/', methods=['GET'], strict_slashes=False)
 def unauthorized() -> str:
     """
     GET /api/v1/unauthorized
@@ -33,7 +33,7 @@ def unauthorized() -> str:
     """
     return abort(401)
 
-@app_views.route('/forbidden/', methods=['Get'], strict_slashes=False)
+@app_views.route('/forbidden/', methods=['GET'], strict_slashes=False)
 def forbidden() -> str:
     """
     GET /api/v1/forbidden
