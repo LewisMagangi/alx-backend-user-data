882dd91ba86987c0a22914520a1d1795f592691d
Liquelaliqour <lewismomanyi34@gmail.com>
2024-11-12 18:09:03 +0300

    Error handler: Unauthorized

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,11 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+@app_views.route('/unauthorized/', methods=['Get'], strict_slashes=False)
+def unauthorized() -> str:
+    """
+    GET /api/v1/unauthorized
+    This endpoint raises a 401 Unauthorized error.
+    """
+    abort(401)
