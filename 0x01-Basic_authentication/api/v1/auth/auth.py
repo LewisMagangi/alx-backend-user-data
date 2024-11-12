4fd8e688b2fb8ed46c810d7fcada8a82803ed686
Liquelaliqour <lewismomanyi34@gmail.com>
2024-11-12 21:06:05 +0300

    Code test and more code documentation

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -3,11 +3,26 @@ from flask import request
 from typing import TypeVar, List
 
 class Auth:
-    def __init__(self, app):
+    def __init__(self):
         pass
+    
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Determines whether authentication is required for a given path.
+        Currently, it returns False by default.
+        """
         return False
+
     def authorization_header(self, request=None) -> str:
+        """
+        Retrieves the Authorization header from the request.
+        Currently, it returns None.
+        """
         return None
+
     def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Returns the current user based on the request.
+        Currently, it returns None.
+        """
         return None
