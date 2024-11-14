cd447d965c3dbbb5a3200da117e90448313313f6
LewisMagangi <lewismomanyi34@gmail.com>
2024-11-14 08:12:18 +0000

    Maintaning Pycodestyle Standards.

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,24 +10,22 @@ class Auth:
     """ Authentication Class
     """
 
-
     def __init__(self):
         """ Initialisation code block
         """
-    
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
         Determines whether authentication is required for a given path.
-    
         Returns:
         - True if path is None
         - True if excluded_paths is None or empty
         - False if path is in excluded_paths (considering slash tolerance)
         """
-        
+
         if path is None or excluded_paths is None or len(path) == 0:
             return True
-        
+
         normalized_path = path.rstrip('/')
 
         for excluded_path in excluded_paths:
