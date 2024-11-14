6d90283a6590150d4c04ccc194f04b6b1749c33f
Liquelaliqour <lewismomanyi34@gmail.com>
2024-11-14 21:28:47 +0300

    Basic - Base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -15,4 +15,11 @@ class BasicAuth(Auth):
         Initialization for BasicAuth (if needed)
         """
 
+    """ A method that returns the Base64 part of the Authorization header for a Basic Authentication.
+    """
+
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        if authorization_header is None or type(authorization_header) is not str or not authorization_header.startswith("Basic "):
+            return None
+        return authorization_header.split("Basic ")[1]
     pass
