b4ea4ec397ef301ee999496bcd514ea8e6e32a98
Liquelaliqour <lewismomanyi34@gmail.com>
2024-11-14 12:02:39 +0300

    Adding Request Validation features

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -4,6 +4,7 @@ Route module for the API
 """
 from os import getenv
 from api.v1.views import app_views
+from api.v1.auth.auth import Auth
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
@@ -12,7 +13,8 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-
+auth = None
+auth = Auth()
 
 @app.errorhandler(404)
 def not_found(error) -> str:
@@ -34,6 +36,19 @@ def forbidden(error) -> str:
     """
     return jsonify({"error": "Forbidden"}), 403
 
+@app.before_request
+def before_request():
+    if Auth is None:
+        pass
+    
+    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    
+    if auth and auth.require_auth(request.path, excluded_paths):
+        if auth.authorization_header(request) is None:
+            abort(401)
+
+        elif auth.current_user(request) is None:
+            abort(403)
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
