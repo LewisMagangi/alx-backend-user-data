7992c68c65cbb5f1d0ae8df140cbd1c916c36b1e
Liquelaliqour <lewismomanyi34@gmail.com>
2024-11-15 16:56:03 +0300

    Updating the session auth class with a create session method.

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,6 +3,7 @@
 """
 from api.v1.auth.auth import Auth
 from flask import request
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
@@ -10,8 +11,20 @@ class SessionAuth(Auth):
     A session Authentication Class.
     """
 
+    user_id_by_session_id = {}
+    """
+    A dictionary to store user_id by session_id
+    """
+
     def __init__(self):
         """ Initialisation code block
         """
 
-    pass
+    def create_session(self, user_id: str = None) -> str:
+        if user_id is None or type(user_id) is not str:
+            return None
+
+        id = uuid4()
+        self.user_id_by_session_id[id] = user_id
+
+        return user_id
