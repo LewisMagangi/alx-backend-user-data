6ad719f36a5a7929da554c388610eed1db84a9d2
Liquelaliqour <lewismomanyi34@gmail.com>
2024-11-18 23:21:06 +0300

    Trial

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -11,6 +11,7 @@ from os import getenv
 from api.v1.app import app
 
 
+'''
 @app.route('/auth_session/login', methods['POST'], strict_slashes=False)
 def login():
     """
@@ -48,3 +49,35 @@ def login():
                 )
             return response
     return jsonify('{error}:{wrong password}'), 401
+'''
+
+
+@app.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def login():
+    """ POST /auth_session/login
+    Return:
+      - Response
+    """
+    user_email = request.form.get('email')
+    user_pwd = request.form.get('password')
+    if not user_email:
+        return jsonify(error="email missing"), 400
+    if not user_pwd:
+        return jsonify(error="password missing"), 400
+    try:
+        user = User.search({"email": user_email})
+    except Exception:
+        return jsonify(error="no user found for this email"), 404
+    if not user:
+        return jsonify(error="no user found for this email"), 404
+    for u in user:
+        if u.is_valid_password(user_pwd):
+            user_id = u.id
+            from api.v1.app import auth
+            session_id = auth.create_session(user_id)
+            response = jsonify(u.to_json())
+            response.set_cookie(getenv('SESSION_NAME'), session_id)
+            return response
+        else:
+            return jsonify(error="wrong password"), 401
+    return jsonify(error="no user found for this email"), 404
