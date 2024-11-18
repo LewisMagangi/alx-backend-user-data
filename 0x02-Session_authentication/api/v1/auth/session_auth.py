806b08fb62e66536202f33fff0ac2e85348f042b
Liquelaliqour <lewismomanyi34@gmail.com>
2024-11-18 17:06:02 +0300

    2nd Creating  an instance method def user_id_for_session_id(self, session_id: str = None) -> str: that returns a User ID based on a Session ID:

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -15,15 +15,30 @@ class SessionAuth(Auth):
     """
     A dictionary to store user_id by session_id
     """
+
     def __init__(self):
         """ Initialisation code block
         """
 
-    pass
-
     def create_session(self, user_id: str = None) -> str:
+        """
+        Creates a new session for a user
+        """
+
         if user_id is None or type(user_id) is not str:
             return None
-        id = uuid4()
-        self.user_id_by_session_id[id] = user_id
-        return user_id
+
+        session_id = str(uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+
+        return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """
+        pass
+        """
+
+        if session_id is None or not isinstance(session_id, str):
+            return None
+
+        return self.user_id_by_session_id.get(session_id)
