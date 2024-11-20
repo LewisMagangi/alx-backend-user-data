2586f32cbac6befbb5416313c9a917f16473ce65
Liquelaliqour <lewismomanyi34@gmail.com>
2024-11-20 17:28:08 +0300

    SQL ALCHEMY CLASS

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from sqlalchemy import Integer, Column, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+    
+    
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    email = Column(String(250), nullable=True, unique=True)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
