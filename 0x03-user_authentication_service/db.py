107f3887638805f4a63d11862a70ec5a96919a13
LewisMagangi <lewismomanyi34@gmail.com>
2024-11-20 15:43:59 +0000

    Echo status change

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -7,7 +7,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 from typing import TypeVar
-from user import Base
+from user import Base, User
 
 
 class DB:
@@ -17,7 +17,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -37,11 +37,10 @@ class DB:
         Returns the created User object.
         """
 
-        from user import User
-
         new_user = User(email=email, hashed_password=hashed_password)
 
-        self._session.add(new_user)
-        self._session.commit()
+        session = self._session
+        session.add(new_user)
+        session.commit()
 
         return new_user
