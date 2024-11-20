5cfc701c7c2cbe2cac905ee669131326822549b8
Liquelaliqour <lewismomanyi34@gmail.com>
2024-11-20 17:59:53 +0300

    Pycodestyle Fix

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -5,10 +5,10 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class User(Base):
     __tablename__ = 'users'
-    
-    
+
     id = Column(Integer, primary_key=True, autoincrement=True)
     email = Column(String(250), nullable=True, unique=True)
     hashed_password = Column(String(250), nullable=False)
