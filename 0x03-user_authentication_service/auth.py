485df2c6543b0b724dc3d4c95abd1a0e03ca8712
Liquelaliqour <lewismomanyi34@gmail.com>
2024-11-20 21:39:02 +0300

    Quick Fix

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -7,9 +7,9 @@ Authentication Module
 import bcrypt
 
 
-def _hash_password(self, password: str) -> bytes:
+def _hash_password(password: str) -> bytes:
     """
     Hashes a password using bcrypt.
-    pyc"""
+    """
 
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
