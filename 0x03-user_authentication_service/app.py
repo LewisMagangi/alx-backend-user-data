d93a6f10d4e1c71a30301c608d237bdf7e8ac97e
Lewis <93725160+LewisMagangi@users.noreply.github.com>
2024-11-21 13:45:21 +0000

    PEP8 FIX

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -89,13 +89,12 @@ def profile():
     session_id = request.cookies.get('session_id')
 
     if not session_id:
-        return jsonify({'error', 'Unauthorized'}), 400
+        return jsonify({'error', 'Unauthorized'}), 403
 
     if user:
         user = AUTH.get_user_from_session_id(session_id)
         return jsonify({'email', user.email}), 200
     abort(403)
-
 
 
 if __name__ == "__main__":
