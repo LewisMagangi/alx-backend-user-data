fcc933c254f2054372d94aff9bc0d20656cc6634
Liquelaliqour <lewismomanyi34@gmail.com>
2024-11-21 12:11:54 +0300

    Moved _hash_password outside of the Auth class as a standalone function.

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -10,6 +10,14 @@ from user import User
 from sqlalchemy.orm.exc import NoResultFound
 
 
+def _hash_password(password: str) -> bytes:
+    """
+    Hashes a password using bcrypt.
+    """
+
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
 class Auth:
     """
     Auth Class to interact with Authentication Database
@@ -22,13 +30,6 @@ class Auth:
 
         self._db = DB()
 
-    def _hash_password(self, password: str) -> bytes:
-        """
-        Hashes a password using bcrypt.
-        """
-
-        return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-
     def register_user(self, email: str, password: str) -> User:
         """
         Registers a new user in the database.
@@ -38,5 +39,5 @@ class Auth:
             self._db.find_user_by(email=email)
             raise ValueError(f"User {email} already exists")
         except NoResultFound:
-            hashed_password = self._hash_password(password)
+            hashed_password = _hash_password(password)
             return self._db.add_user(email, hashed_password)
