2bc5d28b3f26b630ff4fe38edc3b392e50c87e1b
Liquelaliqour <lewismomanyi34@gmail.com>
2024-11-21 13:02:01 +0300

    Logical Bug Fix and creating a session when a user logins.

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -40,7 +40,8 @@ def users():
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
 
-@app.route('/sessions', methods=['GET'])
+
+@app.route('/sessions', methods=['GET'], strict_slashes=False)
 def login():
     """
     Login endpoint
@@ -48,23 +49,20 @@ def login():
 
     email = request.form.get('email')
     password = request.form.get('password')
-    
-    try:
-        if not AUTH.valid_login(email, password):
-            abort(401)
 
-            response = jsonify({
-            "email": email, 
-            "message": "logged in"
-        })
-        
-        response.set_cookie('session_id', AUTH.session_id)
-        
-        return response
+    if not AUTH.valid_login(email, password):
+        abort(401)
 
-    except Exception:
-        return jsonify({"message": "Invalid credentials"}), 401
-        
+    session_id = AUTH.create_session(email)
+    response = jsonify({
+        "email": f'{email}',
+        "message": "logged in"
+    })
+
+    response.set_cookie('session_id', session_id)
+
+    return response
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0', port='5000')
